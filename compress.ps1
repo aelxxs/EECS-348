0cc5433e44d1ace29377248c41acf74ede98b647
Alexis Vielma <alexis.vielma.us@gmail.com>
2023-02-12 19:52:02 -0600

    fix: update path

--- compress.ps1
+++ compress.ps1
@@ -1,7 +1,7 @@
 $lab = Read-Host "Enter the lab number"
 
 
-if (Test-Path .\.bin\Lab0$lab-Vielma.tar) {
+if (Test-Path .\files\Lab0$lab-Vielma.tar) {
     $confirmation = Read-Host "The file Lab0$lab-Vielma will be overwritten. Do you want to continue? (y/n)"
 
     if ($confirmation -eq "n") {
@@ -14,7 +14,7 @@ xcopy .\labs\0$lab .\Lab0$lab-Vielma /E /I /Y
 
 tar -zcvf .\Lab0$lab-Vielma.tar.gz .\Lab0$lab-Vielma
 
-xcopy .\Lab0$lab-Vielma.tar.gz .\.bin /Y
+xcopy .\Lab0$lab-Vielma.tar.gz .\files /Y
 
 Remove-Item .\Lab0$lab-Vielma -Recurse
 Remove-Item .\Lab0$lab-Vielma.tar.gz -Recurse
